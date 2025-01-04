594615d190a496d9b51fb4d61d1fc7fda66dc234
Hathena <Nick1234567890a@gmail.com>
2025-01-04 11:55:37 -0800

    Slight adjustments

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ today = date.today()
 
 @app.route("/")
 def index():
-    return render_template("sample.html")
+    return render_template("acceuil.html")
 
 @app.route("/<name>")
 def say_hi(name):
-    return render_template("sample.html", name=name)
+    return render_template("acceuil.html", name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
